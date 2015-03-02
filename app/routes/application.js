c04ea9c6da93094660c75e4ec7a54f6868e134af
Cole Willsea <coleww@gmail.com>
2015-03-01 17:50:05 -0800

    grab inventory service in app route and render inventory-manager

---
+++ app/routes/application.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  inventory: Ember.inject.service('inventory'),
+  model: function(){
+    // get user inventory data from server?
+    // or user object == inventory?
+  },
+  setupController: function(controller){
+    controller.set('inventory', this.get('inventory'));
+  }
+});
