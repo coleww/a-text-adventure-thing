becc3f76eeb0b5a00a9a90fb1e6b91714b25e509
Cole Willsea <coleww@gmail.com>
2015-03-07 12:24:21 -0800

    add test for inventory#hasKey

--- tests/unit/services/inventory-test.js
+++ tests/unit/services/inventory-test.js
@@ -2,14 +2,22 @@ import {
   moduleFor,
   test
 } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleFor('service:inventory', {
   // Specify the other units that are required for this test.
   // needs: ['service:foo']
 });
 
-// Replace this with your real tests.
 test('it exists', function(assert) {
   var service = this.subject();
   assert.ok(service);
+});
+
+test('it finds keys to room_ids', function(assert) {
+  var service = this.subject();
+  var key = Ember.Object.create({room_id: 1});
+  service.get('keys').pushObject(key);
+  assert.ok(service.hasKey(1));
+  assert.ok(!service.hasKey(2));
 });
