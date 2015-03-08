550b805a9d8afa6d05f4bf86b339ab764fd0da8d
Cole Willsea <coleww@gmail.com>
2015-03-08 13:15:47 -0700

    remove object proxying behavior

--- app/routes/room/item.js
+++ app/routes/room/item.js
@@ -3,24 +3,23 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   inventory: Ember.inject.service('inventory'),
   model: function(params){
-    return this.store.find('item', params.item_id);
-  },
-  setupController: function(controller, model){
-    controller.set('model', model);
-    controller.set('room', this.modelFor('room'));
+    return Ember.RSVP.hash({
+      item: this.store.find('item', params.item_id),
+      room: this.modelFor('room')
+    });
   },
   actions: {
     takeKey: function(key){
       this.get("inventory.keys").pushObject(key);
-      this.currentModel.set("key", null);
+      this.currentModel.item.set("key", null);
     },
     takeThing: function(thing){
       this.get("inventory.things").pushObject(thing);
-      this.currentModel.get("things").removeObject(thing);
+      this.currentModel.item.get("things").removeObject(thing);
     },
     takeTreasure: function(treasure){
       this.get("inventory").incrementProperty("coins", treasure.get("value"));
-      this.currentModel.set("treasure", null);
+      this.currentModel.item.set("treasure", null);
     }
   }
 });
