01bf981934a3de5d3c63fe704c13509aeb4d1207
Cole Willsea <coleww@gmail.com>
2015-03-13 16:45:03 -0700

    fix mirage upgrade issues, specify inverse room in tests

--- tests/acceptance/room-navigation-test.js
+++ tests/acceptance/room-navigation-test.js
@@ -78,7 +78,7 @@ test('room is inaccessible without its key', function(assert){
   assert.expect(3);
   var key = server.create('key', {description: 'a huge key', room_id: 1});
   var item = server.create('item', {name: 'a void', key: key.id});
-  var locked_room = server.create('room', {description: "awesome", locked: true});
+  var locked_room = server.create('room', {description: "awesome", locked: true, north: 2});
   var room = server.create('room', {description: 'a boring hallway', south: locked_room.id, items: [item.id]});
   visit('/rooms/'+room.id);
   click('a.south');
@@ -96,8 +96,8 @@ test('room is inaccessible without its key', function(assert){
 
 test('locked room messages go away', function(assert){
   assert.expect(3);
-  var other_room = server.create('room');
-  var locked_room = server.create('room', {locked: true});
+  var other_room = server.create('room', {south: 3});
+  var locked_room = server.create('room', {locked: true, north: 3});
   var room = server.create('room', {description: 'a boring hallway', south: locked_room.id, north: other_room.id});
   visit('/rooms/'+room.id);
   click('a.south');
