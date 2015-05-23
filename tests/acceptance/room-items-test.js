51479828b2f99c29b5cab1ec6c54c06772da50b8
Cole Willsea <coleww@gmail.com>
2015-05-23 15:28:31 -0700

    update tests to use the fixtures

--- tests/acceptance/room-items-test.js
+++ tests/acceptance/room-items-test.js
@@ -16,46 +16,37 @@ module('Acceptance: RoomItems', {
 
 test('shows linked items in room', function(assert){
   assert.expect(2);
-  var item = server.create('item', {name: 'a lumpy box of chocolates'});
-  server.create('room', {description: 'gazing into the abyss', items: [item.id]});
   visit('/rooms/1');
-  var expected = 'you are gazing into the abyss. you see a lumpy box of chocolates. ';
+  var expected = 'you are in the center of a quiet town. you see a lumpy bag. ';
   andThen(function(){
     assert.equal(find('p.room-description').text().replace(/\s+/g, " "), expected);
-    assert.equal(find('a.item').text(), 'a lumpy box of chocolates');
+    assert.equal(find('a.item').text(), 'a lumpy bag');
   });
 });
 
 test('shows "content" of that item', function(assert){
   assert.expect(1);
-  var item = server.create('item', {name: 'a box', description: 'says "coca-cola" on the side.'});
-  var room = server.create('room', {description: 'gazing into the abyss', items: [item.id]});
-  visit('/rooms/'+room.id+'/item/'+item.id);
+  visit('/rooms/1/item/1');
   andThen(function(){
-    assert.equal(find('p.item-description').text().replace(/^\s+|\s+$/g, ''), 'a box...says "coca-cola" on the side.');
+    assert.equal(find('p.item-description').text().replace(/^\s+|\s+$/g, ''), 'a lumpy bag...smells like weed');
   });
 });
 
 test('links back to parent room', function(assert){
   assert.expect(2);
-  var item = server.create('item');
-  var room = server.create('room', {name: 'darkness', items: [item.id]});
-  visit('/rooms/'+room.id+'/item/'+item.id);
+  visit('/rooms/1/item/1');
   andThen(function(){
     assert.equal(find('a.back').text(), 'back');
-    assert.equal(find('a.back').attr('href'), '/rooms/'+room.id);
+    assert.equal(find('a.back').attr('href'), '/rooms/1');
   });
 });
 
 test('shows keys hidden in that item', function(assert){
   assert.expect(4);
-  var key = server.create('key', {description: 'a rusty old key with a peace sign etched into it'});
-  var item = server.create('item', {key: key.id});
-  var room = server.create('room', {description: 'gazing into the abyss', items: [item.id]});
-  visit('/rooms/'+room.id+'/item/'+item.id);
+  visit('/rooms/1/item/1');
   andThen(function(){
     assert.equal(find('.inventory .key-count').text().replace(/^\s+|\s+$/g, '').replace(/\s+/g, " "), 'Keys: 0');
-    assert.equal(find('a.key').text().replace(/^\s+|\s+$/g, ''), 'a rusty old key with a peace sign etched into it');
+    assert.equal(find('a.key').text().replace(/^\s+|\s+$/g, ''), 'a key');
   });
   click('a.key');
   andThen(function(){
@@ -66,18 +57,15 @@ test('shows keys hidden in that item', function(assert){
 
 test('shows things hidden in that item', function(assert){
   assert.expect(5);
-  var thing = server.create('thing', {name: 'some sort of tool or treasure or idk'});
-  var item = server.create('item', {things: [thing.id]});
-  var room = server.create('room', {items: [item.id]});
-  visit('/rooms/'+room.id+'/item/'+item.id);
+  visit('/rooms/5/item/2');
   andThen(function(){
     assert.equal(find('.inventory .thing-count').text().replace(/^\s+|\s+$/g, '').replace(/\s+/g, " "), 'Things: 0');
-    assert.equal(find('a.thing').text().replace(/^\s+|\s+$/g, ''), 'some sort of tool or treasure or idk');
+    assert.equal(find('a.thing').text().replace(/^\s+|\s+$/g, ''), 'potato');
   });
   click('a.thing');
   andThen(function(){
     assert.equal(find('a.thing').size(), 0);
-    assert.equal(find('ul.things li').text().replace(/^\s+|\s+$/g, ''), 'some sort of tool or treasure or idk');
+    assert.equal(find('ul.things li').text().replace(/^\s+|\s+$/g, ''), 'potato');
     assert.equal(find('.inventory .thing-count').text().replace(/^\s+|\s+$/g, '').replace(/\s+/g, " "), 'Things: 1');
   });
 });
--- tests/acceptance/room-navigation-test.js
+++ tests/acceptance/room-navigation-test.js
@@ -17,56 +17,47 @@ module('Acceptance: RoomNavigation', {
 test('visiting a room', function(assert) {
   assert.expect(1);
 
-  server.create('room', {description: 'in a cold and scary place'});
   visit('/rooms/1');
 
   andThen(function() {
-    assert.equal(find('p.room-description').text().replace(/\s+/g, " "), 'you are in a cold and scary place. ');
+    assert.equal(find('p.room-description').text().replace(/\s+/g, " "), 'you are in the center of a quiet town. you see a lumpy bag. ');
     // ...
   });
 });
 
 test('visiting rooms in a cruel and infinite loop', function(assert) {
-  server.create('room', {description: 'in a cold and scary place', north: 2, south: 3});
-  server.create('room', {description: 'in the north', south: 1, north: 3});
-  server.create('room', {description: 'in the south', north: 1, south: 2});
   visit('/rooms/1');
   click('a.north');
   andThen(function() {
-    assert.equal(find('p.room-description').text().replace(/\s+/g, " "), 'you are in the north. ');
+    assert.equal(find('p.room-description').text().replace(/\s+/g, " "), 'you are at the entrance to town.... ');
   });
   click('a.north');
   andThen(function() {
-    assert.equal(find('p.room-description').text().replace(/\s+/g, " "), 'you are in the south. ');
+    assert.equal(find('p.room-description').text().replace(/\s+/g, " "), 'you are staring at the edge of the planet. You feel deja vu.. you see an old boot. ');
   });
   click('a.north');
   andThen(function() {
-    assert.equal(find('p.room-description').text().replace(/\s+/g, " "), 'you are in a cold and scary place. ');
+    assert.equal(find('p.room-description').text().replace(/\s+/g, " "), 'you are in the center of a quiet town. you see a lumpy bag. ');
   });
 });
 
 test('does not show nonexistent room links', function(assert){
   assert.expect(4);
-  server.create('room', {description: 'a cold and scary place', north: 2});
-  server.create('room', {description: 'northward!', south: 1});
-  visit('/rooms/1');
+  visit('/rooms/5');
   andThen(function(){
-    assert.equal(find('a.north').length, 1);
+    assert.equal(find('a.north').length, 0);
     assert.equal(find('a.west').length, 0);
-    assert.equal(find('a.east').length, 0);
+    assert.equal(find('a.east').length, 1);
     assert.equal(find('a.south').length, 0);
   });
 });
 
 test('shows brief name of linked rooms', function(assert){
   assert.expect(3);
-  server.create('room', {name: 'home', description: 'a cold and scary place', north: 2, south: 3});
-  server.create('room', {name: 'hallway', south: 1, north: 3});
-  server.create('room', {name: 'darkened door', north: 1, south: 2});
   visit('/rooms/1');
   andThen(function(){
-    assert.equal(find('a.north:contains("hallway")').length, 1);
-    assert.equal(find('a.south:contains("darkened door")').length, 1);
+    assert.equal(find('a.north:contains("main gate")').length, 1);
+    assert.equal(find('a.south:contains("main road")').length, 1);
   });
   click('a.north');
   andThen(function(){
@@ -76,36 +67,31 @@ test('shows brief name of linked rooms', function(assert){
 
 test('room is inaccessible without its key', function(assert){
   assert.expect(3);
-  var key = server.create('key', {description: 'a huge key', room_id: 1});
-  var item = server.create('item', {name: 'a void', key: key.id});
-  var locked_room = server.create('room', {description: "awesome", locked: true, north: 2});
-  var room = server.create('room', {description: 'a boring hallway', south: locked_room.id, items: [item.id]});
-  visit('/rooms/'+room.id);
+  visit('/rooms/4');
   click('a.south');
   andThen(function(){
-    assert.equal(find('p.room-description').text().replace(/\s+/g, " "), 'you are a boring hallway. you see a void. ');
+    assert.equal(find('p.room-description').text().replace(/\s+/g, " "), 'you are resting in a lovely garden surrounded by walls.. ');
     assert.equal(find('div.messages').text().replace(/^\s+|\s+$/g, ''), 'You need a key to get in there!');
   });
+  click('a.west');
   click('a.item');
   click('a.key');
+  click('a.east');
   click('a.south');
   andThen(function(){
-    assert.equal(find('p.room-description').text().replace(/\s+/g, " "), 'you are awesome. ');
+    assert.equal(find('p.room-description').text().replace(/\s+/g, " "), 'you are in a shed. ');
   });
 });
 
 test('locked room messages go away', function(assert){
   assert.expect(3);
-  var other_room = server.create('room', {south: 3});
-  var locked_room = server.create('room', {locked: true, north: 3});
-  var room = server.create('room', {description: 'a boring hallway', south: locked_room.id, north: other_room.id});
-  visit('/rooms/'+room.id);
+  visit('/rooms/4');
   click('a.south');
   andThen(function(){
-    assert.equal(find('p.room-description').text().replace(/\s+/g, " "), 'you are a boring hallway. ');
+    assert.equal(find('p.room-description').text().replace(/\s+/g, " "), 'you are resting in a lovely garden surrounded by walls.. ');
     assert.equal(find('div.messages').text().replace(/^\s+|\s+$/g, ''), 'You need a key to get in there!');
   });
-  click('a.north');
+  click('a.west');
   andThen(function(){
     assert.equal(find('div.messages').text().replace(/^\s+|\s+$/g, ''), '');
   });
